9f8afbfa177efaaa68a31248e5edecaa09e7399e
curso00 <curso00@nomail.com>
2024-11-25 12:00:26 +0100

    Antes de meter cambios

---
+++ src/main/java/es/santander/ascender/proyecto09/Proyecto09Application.java
@@ -0,0 +1,13 @@
+package es.santander.ascender.proyecto09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto09Application.class, args);
+	}
+
+}
---
+++ src/test/java/es/santander/ascender/proyecto09/Proyecto09ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.santander.ascender.proyecto09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto09ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
